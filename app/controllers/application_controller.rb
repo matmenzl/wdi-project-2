b67e782331c941b261ba7d75a64ba8be474a87c6
matmenzl <info@mathiasmenzl.ch>
2016-04-22 17:38:32 +0100

    added before_filter for authenctication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user!, except: [:home, :signup, :register]
+
   protected
   def after_sign_in_path_for(resource)
     root_path
@@ -11,8 +13,7 @@ class ApplicationController < ActionController::Base
   class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-
+  protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username, :first_name, :last_name, :street, :zip, :city) }
     end
