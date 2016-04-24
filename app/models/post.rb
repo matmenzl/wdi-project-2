85b8d1254675a556a0d9c34fbb8a852e9d6b5f62
matmenzl <info@mathiasmenzl.ch>
2016-04-24 14:57:59 +0100

    added comments to post-show-page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  acts_as_commentable
 
   validates :title, presence: true, length: { in: 5..500 }
   validates :image, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :posts
+  resources :comments, only: [:create, :destroy]
 
   root 'statics#home'
 end
