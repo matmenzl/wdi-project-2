a3ff2d6e0d0ab19c348ac0cbabd41fd9990782da
matmenzl <info@mathiasmenzl.ch>
2016-04-26 11:33:36 +0100

    fixed registration bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_filter :authenticate_user!, except: [:home, :signup, :register]
 
   protected
@@ -10,12 +10,9 @@ class ApplicationController < ActionController::Base
     root_path
   end
   
-  class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username, :first_name, :last_name, :street, :zip, :city, :longitude, :latitude) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username, :user_image, :remote_user_image_url, :first_name, :last_name, :street, :zip, :city, :longitude, :latitude) }
     end
-  end
 end
